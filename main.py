568aec06254fbca583dcb17f3a3eb4a817257a8b
Harika2225 <harikasree2k@gmail.com>
2023-11-27 12:44:47 +0530

    check python code is working or not

---
+++ main.py
@@ -0,0 +1,32 @@
+# to check if python code works fine
+
+import openai
+
+def summarize_code(code):
+    openai.api_key = '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'  
+    prompt = f"Summarize the following code snippet: "+code
+    
+    response = openai.completions.create(
+        model='text-davinci-003',  
+        prompt=prompt,
+        max_tokens=100, 
+        temperature=0.5, 
+        n=1,  
+        timeout=10,  
+    )
+
+    if response and response.choices:
+        return response.choices[0].text.strip()
+    
+    return response
+
+code = '''
+def multiply(a, b):
+    return a * b
+
+result = multiply(5, 3)
+print(result)
+'''
+
+summary = summarize_code(code)
+print(summary)
