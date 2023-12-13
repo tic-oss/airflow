833451993e5a43f196a0954a9370cbc4c3cd04ca
Harika2225 <harikasree2k@gmail.com>
2023-12-13 11:58:40 +0530

    optimized code for unstructured dag

--- dags/unstructured_dag.py
+++ dags/unstructured_dag.py
@@ -6,97 +6,54 @@ import time
 from datetime import datetime
 import os
 
-dag = DAG('unstructured_data', start_date=datetime(2023,12,12), description='Determine the topic of the text')
+dag = DAG('unstructured_data', start_date=datetime(2023,12,12), description='Data Pipeline for unstructured data')
+
+def openai_interaction(file_path, prompt, output_file):
+    openai.api_key = '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'
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
+    with open(file_path, 'r') as file:
+        code = file.read()
+    if len(code.split()) > 4097:
+        code = ' '.join(code.split()[:4097])
+    try:
+        response = openai.completions.create(
+            model="text-davinci-003",
+            prompt=prompt.format(code),
+            max_tokens=2000,
+            temperature=0.7,
+            n=1,
+            timeout=10,
+        )
+        if response and response.choices:
+            with open(output_file, 'w') as output:
+                output.write(str(response.choices[0].text.strip()))
+            return response.choices[0].text.strip()
+    except openai.RateLimitError as e:
+        print(f"Rate limit exceeded. Waiting for {e.retry_after} seconds.")
+        time.sleep(e.retry_after)
+        return openai_interaction(file_path, prompt, output_file)
+    except Exception as ex:
+        print(f"An error occurred: {ex}")
+        with open(output_file, 'w') as output:
+            output.write("Error: Unable to generate a summary.")
 
 def fetch_topic(file_path, output_file):
-    openai.api_key = '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'
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    with open(file_path, 'r') as file:
-        code = file.read()
-    if len(code.split()) > 4097:
-        code = ' '.join(code.split()[:4097])
-    prompt = f"what is the topic of the text: {code}"
-    try:
-        response = openai.completions.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            max_tokens=2000,  
-            temperature=0.7,
-            n=1,
-            timeout=10,
-        )
-        if response and response.choices:
-            print('######################',response.choices[0].text.strip())
-            with open(output_file, 'w') as output:
-                # output.write(response)
-                output.write(str(response.choices[0].text.strip()))
-            return response.choices[0].text.strip()
-    except openai.RateLimitError as e:
-        print(f"Rate limit exceeded. Waiting for {e.retry_after} seconds.")
-        time.sleep(e.retry_after)
-        return fetch_topic(file_path, output_file)
-    return "Error: Unable to generate a summary."
+    prompt = 'What is the topic of the text: {}'
+    return openai_interaction(file_path, prompt, output_file)
 
 def create_questions(file_path, output_file):
-    openai.api_key = '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'
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    with open(file_path, 'r') as file:
-        code = file.read()
-    if len(code.split()) > 4097:
-        code = ' '.join(code.split()[:4097])
-    prompt = f"create 10 questions from the text: {code}"
-    try:
-        response = openai.completions.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            max_tokens=2000,  
-            temperature=0.7,
-            n=1,
-            timeout=10,
-        )
-        if response and response.choices:
-            print('###################################')
-            with open(output_file, 'w') as output:
-                output.write(str(response.choices[0].text.strip()))
-            return response.choices[0].text.strip()
-    except openai.RateLimitError as e:
-        print(f"Rate limit exceeded. Waiting for {e.retry_after} seconds.")
-        time.sleep(e.retry_after)
-        return fetch_topic(file_path, output_file)
-    return "Error: Unable to generate a summary."
+    prompt = 'create 10 questions from the text: {}'
+    return openai_interaction(file_path, prompt, output_file)
 
 def generate_answers(file_path, output_file, **kwargs):
     ti = kwargs['ti']
     questions_output = ti.xcom_pull(task_ids='create_questions_task')
-    openai.api_key = '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'
-    os.makedirs(os.path.dirname(output_file), exist_ok=True)
-    with open(file_path, 'r') as file:
-        code = file.read()
-    if len(code.split()) > 4097:
-        code = ' '.join(code.split()[:4097])
-    prompt = f"generate answers for the questions : {questions_output}"
-    try:
-        response = openai.completions.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            max_tokens=2000,  
-            temperature=0.7,
-            n=1,
-            timeout=10,
-        )
-        if response and response.choices:
-            print('###################################')
-            with open(output_file, 'w') as output:
-                output.write(str(response.choices[0].text.strip()))
-            return response.choices[0].text.strip()
-    except openai.RateLimitError as e:
-        print(f"Rate limit exceeded. Waiting for {e.retry_after} seconds.")
-        time.sleep(e.retry_after)
-        return fetch_topic(file_path, output_file)
-    return "Error: Unable to generate a summary."
+    prompt = f'generate answers for the questions : {questions_output}'
+    return openai_interaction(file_path, prompt, output_file)
 
 input_file_path = '/home/harika/wikidata/content.txt'
 output_folder = '/home/harika/wikidata/unstructured_dag'
+
 fetch_topic_task = PythonOperator(
     task_id='fetch_topic_task',
     python_callable=fetch_topic,
